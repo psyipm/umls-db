308c140297f120e7c729645d2fc14a8788c98b41
Igor Malinovskiy <psy.ipm@gmail.com>
2014-11-03 16:24:23 +0200

    поиск по карте

--- 37.sql
+++ 37.sql
@@ -9,3 +9,90 @@ ADD COLUMN last_update timestamp without time zone NOT NULL DEFAULT now();
 COMMENT ON COLUMN currency.bid IS 'Цена покупки за единицу валюты по отношению к USD';
 COMMENT ON COLUMN currency.ask IS 'Цена продажи за единицу валюты по отношению к USD';
 COMMENT ON COLUMN currency.last_update IS 'Дата последнего обновления';
+
+
+CREATE OR REPLACE FUNCTION convert_to_usd
+	(
+	  p_amount double precision,
+	  p_currency_id integer 
+	)
+	RETURNS double precision AS
+	$$
+	DECLARE
+	  v_exchange_rate double precision;
+	BEGIN
+	  SELECT bid
+	  into v_exchange_rate
+	  FROM currency
+	  WHERE id = p_currency_id;
+
+	  return p_amount*v_exchange_rate;
+	END;
+	$$
+	LANGUAGE 'plpgsql';
+
+
+create view v_re_object_by_price as
+
+SELECT o.id,
+   o.transaction_type,
+   o.category_id,
+   o.country_id,
+   o.region_id,
+   o.city_id,
+   o.cityarea_id,
+   o.geo_lat,
+   o.geo_long,
+   o.street,
+   o.build,
+   o.price,
+   o.currency_id,
+   o.price_for,
+   o.reff,
+   o.agent_id,
+   o.data_create,
+   o.data_renew,
+   o.data_action,
+   o.data_unact,
+   o.data_sold,
+   o.data_del,
+   o.is_del,
+   o.is_umls_user,
+   o.published,
+   o.count_view,
+   o.meta_keyword,
+   o.meta_description,
+   o.meta_title,
+   o.tags,
+   o.info_short,
+   o.info_full,
+   o.video_link,
+   o.state,
+   o.area_gen,
+   o.area_res,
+   o.measure_id,
+   o.commission_total,
+   o.commission_total_type,
+   o.commission_partner,
+   o.commission_partner_type,
+   o.price_sold,
+   o.currency_sold,
+   tt.name AS transaction_type_name,
+   c.name AS category_name,
+   ct.city AS city_name,
+   cta.name AS cityarea_name,
+   cr.short_name AS currency,
+   o.price / o.area_res::double precision AS price_for_sqm
+  FROM re_object o
+    JOIN transaction_types tt ON o.transaction_type = tt.type_code
+    JOIN re_category c ON o.category_id = c.id AND c.parent <> 0
+    JOIN geo_city ct ON o.city_id = ct.id
+    LEFT JOIN geo_cityarea cta ON o.cityarea_id = cta.id
+    JOIN currency cr ON o.currency_id = cr.id
+   where 
+   convert_to_usd(o.price, o.currency_id)
+   between
+       convert_to_usd(CAST(get_var('price_min') AS DOUBLE PRECISION), CAST(get_var('currency_id') AS INTEGER))
+       and
+       convert_to_usd(CAST(get_var('price_max') AS DOUBLE PRECISION), CAST(get_var('currency_id') AS INTEGER));
+    
